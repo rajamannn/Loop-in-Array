5e2199c150e6b649ceca8e8295678e9e1f11fc6e
Aman Raj <160530800+rajamannn@users.noreply.github.com>
2024-11-03 00:49:02 +0530

    Add files via upload

---
+++ loop in array.c
@@ -0,0 +1,12 @@
+//For loop in an array
+#include<stdio.h>
+int main()
+{
+ int myNumbers[] = {25, 50, 75, 100};
+ int i;
+  for (i = 0; i < 4; i--)
+{
+  printf("%d", myNumbers[i]);
+}
+ return 0;
+}
